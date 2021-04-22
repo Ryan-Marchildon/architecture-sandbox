eeb18975bbe39bd4eb44a44c9b98b33eedcdf150
Ryan Marchildon <r.p.marchildon@gmail.com>
2021-04-21 21:51:32 -0400

    Add postgres to config

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,8 +24,38 @@ def session(in_memory_db):
     clear_mappers()
 
 
+def wait_for_postgres_spinup(engine):
+    deadline = time.time() + 10
+    while time.time() < deadline:
+        try:
+            return engine.connect()
+        except OperationalError:
+            time.sleep(0.5)
+    pytest.fail("Postgres never spun up")
+
+
+@pytest.fixture(scope="session")
+def postgres_db():
+    engine = create_engine(config.get_postgres_uri())
+    wait_for_postgres_spinup(engine)
+    metadata.create_all(engine)
+    return engine
+
+
 @pytest.fixture
-def add_stock(session):
+def postgres_session(postgres_db):
+    start_mappers()
+    yield sessionmaker(bind=postgres_db)()
+    clear_mappers()
+
+
+@pytest.fixture
+def add_stock(postgres_session):
+    """
+    Adds batches to the postgres DB;
+    for running e2e tests.
+
+    """
 
     # NOTE: we use 'yield' to specify teardown code without callbacks.
     # This is a feature of pytest fixtures, see:
@@ -36,37 +66,37 @@ def add_stock(session):
 
     def _add_stock(lines):
         for ref, sku, qty, eta in lines:
-            session.execute(
+            postgres_session.execute(
                 "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
                 " VALUES (:ref, :sku, :qty, :eta)",
                 dict(ref=ref, sku=sku, qty=qty, eta=eta),
             )
-            batch_id = session.execute(
+            batch_id = postgres_session.execute(
                 "SELECT id FROM batches WHERE reference=:ref AND sku=:sku",
                 dict(ref=ref, sku=sku),
             )[0][0]
             batches_added.add(batch_id)
             skus_added.add(sku)
-        session.commit()
+        postgres_session.commit()
 
     yield _add_stock  # NOTE: yields from fixtures produce only one value (will not iterate)
 
     # this teardown code runs after the test has finished (yield is 'finalized')
     for batch_id in batches_added:
-        session.execute(
+        postgres_session.execute(
             "DELETE FROM allocations WHERE batch_id=:batch_id",
             dict(batch_id=batch_id),
         )
-        session.execute(
+        postgres_session.execute(
             "DELETE FROM batches WHERE id=:batch_id",
             dict(batch_id=batch_id),
         )
     for sku in skus_added:
-        session.execute(
+        postgres_session.execute(
             "DELETE FROM order_lines WHERE sku=:sku",
             dict(sku=sku),
         )
-    session.commit()
+    postgres_session.commit()
 
 
 def wait_for_webapp_spinup():
