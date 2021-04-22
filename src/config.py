6e01bc274a4833ed4195f259bf328b0b741566aa
Ryan Marchildon <r.p.marchildon@gmail.com>
2021-04-21 21:18:09 -0400

    Add postgres uri to config

--- src/config.py
+++ src/config.py
@@ -5,3 +5,10 @@ def get_api_url():
     host = os.environ.get("API_HOST", "localhost")
     port = 5005 if host == "localhost" else 80
     return f"http://{host}:{port}"
+
+
+def get_postgres_uri():
+    host = os.environ.get("DB_HOST", "localhost")
+    port = 54321 if host == "localhost" else 5432
+    password = os.environ.get("DB_PASSWORD", "myexposedpassword123")
+    user, db_name = "allocation", "allocation"
