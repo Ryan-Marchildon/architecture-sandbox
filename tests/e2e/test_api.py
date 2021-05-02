3b0bac2cfd5e03b442d80f800dfa8b181301deee
Ryan Marchildon <r.p.marchildon@gmail.com>
2021-05-02 19:04:20 -0400

    Add post to add batch
    By adding an API endpoint to add batches, we decouple the
    e2e tests from its direct dependency on the database and
    hardcoded SQL queries (that it had via add_stock).

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -26,21 +26,25 @@ def random_orderid(name=None):
     return f"order-{create_tag(name)}{random_suffix()}"
 
 
+def post_to_add_batch(ref, sku, qty, eta):
+    url = config.get_api_url()
+    r = requests.post(
+        f"{url}/batches", json={"ref": ref, "sku": sku, "qty": qty, "eta": eta}
+    )
+    assert r.status_code == 201
+
+
 # NOTE: usefixtures() lets us run a fixture even if we don't need
 # this fixture object within our test function
 @pytest.mark.usefixtures("restart_api")
-def test_happy_path_returns_201_and_allocated_batch(add_stock):
+def test_happy_path_returns_201_and_allocated_batch():
     sku, othersku = random_sku(), random_sku("other")
     earlybatch = random_batchref(1)
     laterbatch = random_batchref(2)
     otherbatch = random_batchref(3)
-    add_stock(
-        [
-            (laterbatch, sku, 100, "2011-01-02"),
-            (earlybatch, sku, 100, "2011-01-01"),
-            (otherbatch, othersku, 100, None),
-        ]
-    )
+    post_to_add_batch(laterbatch, sku, 100, "2011-01-02")
+    post_to_add_batch(earlybatch, sku, 100, "2011-01-01")
+    post_to_add_batch(otherbatch, othersku, 100, None)
     data = {"orderid": random_orderid(), "sku": sku, "qty": 3}
     url = config.get_api_url()
     r = requests.post(f"{url}/allocate", json=data)
@@ -58,17 +62,12 @@ def test_unhappy_path_returns_400_and_error_message():
     assert r.json()["message"] == f"Invalid sku {unknown_sku}"
 
 
-def post_to_add_batch(batch, sku, qty, eta):
-    pass
-
-
 @pytest.mark.usefixtures("postgres_db")
 @pytest.mark.usefixtures("restart_api")
-def test_deallocate(add_stock):
+def test_deallocate():
     sku, order1, order2 = random_sku(), random_orderid(), random_orderid()
     batch = random_batchref()
-    add_stock([(batch, sku, 100, "2011-01-02")])
-
+    post_to_add_batch(batch, sku, 100, "2011-01-02")
     url = config.get_api_url()
     # fully allocate
     r = requests.post(
