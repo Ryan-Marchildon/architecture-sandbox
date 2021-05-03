ba03539d5f6fd9bed2f9c08757be4ec38b4dbbc0
Ryan Marchildon <r.p.marchildon@gmail.com>
2021-05-02 20:11:58 -0400

    Update entrypoint to use uow

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -1,31 +1,23 @@
 from datetime import datetime
 
 from flask import Flask, jsonify, request
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 from src.allocation import config
 from src.allocation.domain import model
-from src.allocation.adapters import orm, repository
-from src.allocation.service_layer import services
+from src.allocation.adapters import orm
+from src.allocation.service_layer import services, unit_of_work
 
 orm.start_mappers()
-get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 
 app = Flask(__name__)
 
 
 @app.route("/allocate", methods=["POST"])
 def allocate_endpoint():
-    session = get_session()
-    repo = repository.SqlAlchemyRepository(session)
+    uow = unit_of_work.SqlAlchemyUnitOfWork()
     try:
         batchref = services.allocate(
-            request.json["orderid"],
-            request.json["sku"],
-            request.json["qty"],
-            repo,
-            session,
+            request.json["orderid"], request.json["sku"], request.json["qty"], uow
         )
     except (model.OutOfStock, services.InvalidSku) as e:
         return jsonify({"message": str(e)}), 400
@@ -35,15 +27,10 @@ def allocate_endpoint():
 
 @app.route("/deallocate", methods=["POST"])
 def deallocate_endpoint():
-    session = get_session()
-    repo = repository.SqlAlchemyRepository(session)
+    uow = unit_of_work.SqlAlchemyUnitOfWork()
     try:
         batchref = services.deallocate(
-            request.json["orderid"],
-            request.json["sku"],
-            request.json["qty"],
-            repo,
-            session,
+            request.json["orderid"], request.json["sku"], request.json["qty"], uow
         )
     except (model.OutOfStock, services.InvalidSku) as e:
         return jsonify({"message": str(e)}), 400
@@ -53,18 +40,12 @@ def deallocate_endpoint():
 
 @app.route("/batches", methods=["POST"])
 def add_batch():
-    session = get_session()
-    repo = repository.SqlAlchemyRepository(session)
+    uow = unit_of_work.SqlAlchemyUnitOfWork()
     eta = request.json["eta"]
     if eta is not None:
         eta = datetime.fromisoformat(eta).date()
     services.add_batch(
-        request.json["ref"],
-        request.json["sku"],
-        request.json["qty"],
-        eta,
-        repo,
-        session,
+        request.json["ref"], request.json["sku"], request.json["qty"], eta, uow
     )
     return "OK", 201
 
