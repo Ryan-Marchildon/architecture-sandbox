15b293c4fed60fdfbdb81db384f41bf0f6b3ba07
Ryan Marchildon <r.p.marchildon@gmail.com>
2021-06-05 18:08:16 -0400

    Remove deprecated events

--- src/allocation/domain/events.py
+++ src/allocation/domain/events.py
@@ -11,31 +11,3 @@ class Event:
 @dataclass
 class OutOfStock(Event):
     sku: str
-
-
-@dataclass
-class BatchCreated(Event):
-    ref: str
-    sku: str
-    qty: int
-    eta: Optional[date] = None
-
-
-@dataclass
-class AllocationRequest(Event):
-    orderid: str
-    sku: str
-    qty: int
-
-
-@dataclass
-class DeallocationRequest(Event):
-    orderid: str
-    sku: str
-    qty: int
-
-
-@dataclass
-class BatchQuantityChanged(Event):
-    ref: str
-    qty: int
